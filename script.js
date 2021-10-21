105125dabc188366ae0592520e4056b03632e9b1
TheEdamCheese <>
2021-10-21 16:25:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showHex(){
+      var hex =  document.querySelector("#color_hex_value"),
+          p = document.querySelector("p");
+    
+      p.style.color = document.querySelector("#color_picker").value;
+    }
